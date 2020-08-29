ffe3d1a2e39fb366896990afe97d005d37c32ca6
Фёдор <zuev.f.s@gmail.com>
2020-08-29 10:10:50 +1000

    Init

---
+++ Assets/Scripts/CheckThiefe.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckThiefe : MonoBehaviour
+{
+    [SerializeField] private AudioSource _cignalizationAudio;
+    [SerializeField] private Animator _cignalizationAnimator;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _cignalizationAudio.Play();
+        _cignalizationAnimator.SetBool("isInHome", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _cignalizationAudio.Stop();
+        _cignalizationAnimator.SetBool("isInHome", false);
+    }
+}
