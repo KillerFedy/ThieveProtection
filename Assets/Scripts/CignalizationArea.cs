a18b639e68a70e95899763759233ec20e9211756
Фёдор <zuev.f.s@gmail.com>
2020-09-03 21:11:11 +1000

    FixMistakes

---
+++ Assets/Scripts/CignalizationArea.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CignalizationArea : MonoBehaviour
+{
+    [SerializeField] private AudioSource _cignalizationAudio;
+    [SerializeField] private Animator _cignalizationAnimator;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.TryGetComponent<Thieve>(out Thieve thieve))
+        {
+            _cignalizationAudio.Play();
+            _cignalizationAnimator.SetBool("isInHome", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.TryGetComponent<Thieve>(out Thieve thieve))
+        {
+            _cignalizationAudio.Stop();
+            _cignalizationAnimator.SetBool("isInHome", false);
+        }
+    }
+}
---
+++ Assets/Scripts/Thieve.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thieve : MonoBehaviour
+{
+    [SerializeField] private Transform _startPoint;
+    [SerializeField] private Transform _endPoint;
+    [SerializeField] private int _speed;
+    private Transform _target;
+
+    private void Start()
+    {
+        _target = _endPoint;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _target.position, Time.deltaTime * _speed);
+        if(transform.position == _endPoint.position)
+        {
+            _target = _startPoint;
+        }
+        else if (transform.position == _startPoint.position)
+        {
+            _target = _endPoint;
+        }
+    }
+}
